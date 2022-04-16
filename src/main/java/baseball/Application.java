e5864c97f4d2859fb7dc0b35993fc58df16bac7a
Minjae Kim <minjae.kim@mytrum.com>
2022-04-17 00:19:27 +0900

    refactor: startGame 메소드 기능 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,8 +4,6 @@ import baseball.feature.Answer;
 import baseball.handler.CustomHandler;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Map;
-
 public class Application {
 
     private static final int NUMBER_OF_DIGIT = 3;
@@ -13,52 +11,21 @@ public class Application {
     private static final String RESTART = "1";
     private static final String END = "2";
 
-
-
     public static void main(String[] args) {
         while (isContinue) {
             startGame();
-
             gameEndCheck();
         }
     }
 
     private static void startGame() {
-        //TODO : Refactoring
-        Map<Integer, Integer> answer = new Answer(NUMBER_OF_DIGIT).getAnswer();
+        Answer answer = new Answer(NUMBER_OF_DIGIT);
         boolean isWrongAnswer = true;
-
-        while (isWrongAnswer){
-            System.out.println("please input 3 digit of number");
-            String input = Console.readLine();
-
+        while (isWrongAnswer) {
+            String input = input();
             CustomHandler.invalidCheck(input, NUMBER_OF_DIGIT);
-
-            int ballCount = 0;
-            int strikeCount = 0;
-
-            for (int i = 0; i < input.length(); i++) {
-                int value = Integer.parseInt(String.valueOf(input.charAt(i)));
-                if (answer.get(i).equals(value)) {
-                    strikeCount++;
-                } else if (answer.containsValue(value)) {
-                    ballCount++;
-                }
-            }
-
-            if (strikeCount == answer.size()) {
-                System.out.println(strikeCount+"스트라이크");
-                System.out.println(answer.size()+"개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                isWrongAnswer = false;
-            } else if (ballCount + strikeCount == 0) {
-                System.out.println("낫싱");
-            } else if (ballCount != 0 && strikeCount != 0) {
-                System.out.println(ballCount+"볼 "+strikeCount+"스트라이크");
-            } else if (ballCount != 0 && strikeCount == 0) {
-                System.out.println(ballCount+"볼");
-            } else if (ballCount == 0 && strikeCount != 0) {
-                System.out.println(strikeCount+"스트라이크");
-            }
+            answer.calculateScore(input);
+            isWrongAnswer = answer.getIsWrongAnswer();
         }
     }
 
@@ -68,5 +35,10 @@ public class Application {
         if (input.equals(END) || !input.equals(RESTART)) {
             isContinue = false;
         }
+    }
+
+    private static String input() {
+        System.out.println("please input 3 digit of number");
+        return Console.readLine();
     }
 }
--- src/main/java/baseball/feature/Answer.java
+++ src/main/java/baseball/feature/Answer.java
@@ -14,6 +14,9 @@ public class Answer {
     private static final int DEFAULT = 3;
 
     private final HashMap<Integer, Integer> answer;
+    private int strikeCount;
+    private int ballCount;
+    private boolean isWrongAnswer = true;
 
     public Answer() {
         this(DEFAULT);
@@ -30,11 +33,75 @@ public class Answer {
         return this.answer;
     }
 
+    public boolean getIsWrongAnswer() {
+        return this.isWrongAnswer;
+    }
+
     private int makeNumber() {
         int value = Randoms.pickNumberInRange(MIN, MAX);
         if (this.answer.containsValue(value)) {
             return makeNumber();
         }
         return value;
+    }
+
+    public void calculateScore(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            int value = Integer.parseInt(String.valueOf(input.charAt(i)));
+            if (answer.get(i).equals(value)) {
+                strikeCount++;
+            } else if (answer.containsValue(value)) {
+                ballCount++;
+            }
+        }
+        printScore();
+    }
+
+    private void printScore() {
+        isCorrect();
+        isNothing();
+        isBallAndStrike();
+        isBall();
+        isStrike();
+    }
+
+    private void isCorrect() {
+        if (strikeCount == answer.size()) {
+            System.out.println(strikeCount + "스트라이크");
+            System.out.println(answer.size() + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            isWrongAnswer = false;
+        }
+    }
+
+    private void isNothing() {
+        if (ballCount + strikeCount == 0) {
+            System.out.println("낫싱");
+        }
+    }
+
+    private void isBallAndStrike() {
+        if (ballCount != 0 && strikeCount != 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+            resetScore();
+        }
+    }
+
+    private void isBall() {
+        if (ballCount != 0 && strikeCount == 0) {
+            System.out.println(ballCount + "볼");
+            resetScore();
+        }
+    }
+
+    private void isStrike() {
+        if (ballCount == 0 && strikeCount != 0) {
+            System.out.println(strikeCount + "스트라이크");
+            resetScore();
+        }
+    }
+
+    private void resetScore() {
+        this.ballCount = 0;
+        this.strikeCount = 0;
     }
 }
