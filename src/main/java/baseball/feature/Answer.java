c4e029cdf7b537953dbbe577756658ce4ffa1b27
Minjae Kim <minjae.kim@mytrum.com>
2022-04-16 10:03:34 +0900

    feat: 난수 생성 기능 구현

---
+++ src/main/java/baseball/feature/Answer.java
@@ -0,0 +1,37 @@
+package baseball.feature;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+
+public class Answer {
+
+    private static final int MIN = 1;
+    private static final int MAX = 9;
+    private static final int DEFAULT = 3;
+
+    private HashMap<Integer, Integer> answer;
+
+    public Answer() {
+        this(DEFAULT);
+    }
+
+    public Answer(Integer number) {
+        answer = new HashMap<>();
+        for (int i = 0; i < number; i++) {
+            this.answer.put(i, makeNumber());
+        }
+    }
+
+    public HashMap<Integer, Integer> getAnswer() {
+        return this.answer;
+    }
+
+    private int makeNumber() {
+        int value = Randoms.pickNumberInRange(MIN, MAX);
+        if (this.answer.containsValue(value)) {
+            return makeNumber();
+        }
+        return value;
+    }
+}
---
+++ src/test/java/baseball/feature/AnswerTest.java
@@ -0,0 +1,43 @@
+package baseball.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnswerTest {
+
+    @DisplayName("Answer 클래스 중복 데이터 생성 테스트")
+    @Test
+    void answerDuplicationTest() {
+
+        //given:
+        Map<Integer, Integer> map = new Answer().getAnswer();
+
+        //when:
+        Set<Integer> set = new HashSet<>(map.values());
+
+        //then:
+        assertThat(set)
+                .hasSize(3);
+    }
+
+    @DisplayName("Answer 클래스 난수 생성 테스트")
+    @Test
+    void numberOfAnswerTest() {
+
+        //given:
+        Map<Integer, Integer> map = new Answer(5).getAnswer();
+
+        //when:
+        Set<Integer> set = new HashSet<>(map.values());
+
+        //then:
+        assertThat(set)
+                .hasSize(5);
+    }
+}
